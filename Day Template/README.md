a5d5b3e9a4076ad748173802f2deeda13b0f6131
Ben Merryman <ben.merryman@gmail.com>
2023-12-07 13:46:25 -0700

    ➕ Created day template

---
+++ Day Template/README.md
@@ -0,0 +1,1 @@
+# Day xx of Advent of Code 2023
---
+++ Day Template/day_num.py
@@ -0,0 +1,9 @@
+"""
+Solving Day xx of Advent of Code 2023
+"""
+
+with open("Day xx/input.txt", encoding="ASCII") as f:
+    lines = f.read().splitlines()
+
+for line in lines:
+    print(line)
