bbe2bae1bf42942cf4074bfb4112d651be1a3c9c
Ben Merryman <ben.merryman@gmail.com>
2023-12-07 13:29:56 -0700

    Day 2 Part 1 In Progress

---
+++ Day 2/day2.py
@@ -0,0 +1,37 @@
+"""
+Solution for day 2 of Advent of Code.
+"""
+
+with open("Day 2/testInputPart1.txt", encoding="ASCII") as f:
+    games = f.read().splitlines()
+# Max cubes for each color
+MAX_CUBES = {"red": 12, "green": 13, "blue": 14}
+VALID_GAME_SUM = 0
+GAME_NUMBER = 1
+for game in games:
+    VALID_GAME = True
+    gameCubesMax = {}
+    print(game)
+    gameGrabs = game.split(":")[1].split(";")
+    for grabsCounts in gameGrabs:
+        colorsCounts = grabsCounts.split(",")
+        for cubesColor in colorsCounts:
+            # print(cubesColor)
+            cubesColorSplit = cubesColor.strip().split(" ")
+            cubes = int(cubesColorSplit[0])
+            currentColor = cubesColorSplit[1]
+            if currentColor not in gameCubesMax:
+                gameCubesMax[currentColor] = cubes
+            elif gameCubesMax[currentColor] < cubes:
+                gameCubesMax[currentColor] = cubes
+            if cubes > MAX_CUBES[currentColor]:
+                VALID_GAME = False
+                break
+
+    print(f"Game Number: {GAME_NUMBER} | Valid game: {VALID_GAME}")
+    print(gameCubesMax)
+    if VALID_GAME:
+        VALID_GAME_SUM += GAME_NUMBER
+    GAME_NUMBER += 1
+
+print(f"Valid game sum: {VALID_GAME_SUM}")
